1a336bdab9208d289aa9a6e7243bce91e32d56be
JongHoon <qnm541@gmail.com>
2023-07-25 16:55:28 +0900

    add Mood enum

---
+++ OhMyMood/Mood.swift
@@ -0,0 +1,14 @@
+//
+//  Mood.swift
+//  OhMyMood
+//
+//  Created by JongHoon on 2023/07/25.
+//
+
+enum Mood: Int, CaseIterable {
+    case veryGood
+    case good
+    case soso
+    case bad
+    case veryBad
+}
