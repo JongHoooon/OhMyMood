2017cb49d2faadd8a3d3f5a98031c053d2bd9dcc
JongHoon <qnm541@gmail.com>
2023-07-25 18:01:04 +0900

    add pull down button

--- OhMyMood/Mood.swift
+++ OhMyMood/Mood.swift
@@ -11,4 +11,14 @@ enum Mood: Int, CaseIterable {
     case soso
     case bad
     case veryBad
+    
+    var name: String {
+        switch self {
+        case .veryGood:     return "완전행복지수"
+        case .good:         return "적당미소지수"
+        case .soso:         return "그냥그냥지수"
+        case .bad:          return "좀속상한지수"
+        case .veryBad:      return "많이슬픈지수"
+        }
+    }
 }
--- OhMyMood/WriteViewController.swift
+++ OhMyMood/WriteViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class WriteViewController: UIViewController {
     
+    @IBOutlet var emojiButtons: [UIButton]!
+    
     private var countsOfEmojiTapped = [Int](
         repeating: 0,
         count: Mood.allCases.count
@@ -17,10 +19,59 @@ final class WriteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configButtons()
+    }
+    
+    private func configButtons() {
+        emojiButtons.forEach { [weak self] button in
+            
+            let plusOne = UIAction(
+                title: "1점+",
+                image: UIImage(systemName: "bitcoinsign.circle")
+            ) { _ in
+                self?.addAndPrintScore(tag: button.tag)
+            }
+            let plusFive = UIAction(
+                title: "5점+",
+                image: UIImage(systemName: "bitcoinsign.circle")
+            ) { _ in
+                self?.addAndPrintScore(tag: button.tag, score: 5)
+            }
+            let plusTen = UIAction(
+                title: "10점+",
+                image: UIImage(systemName: "bitcoinsign.circle")
+            ) { _ in
+                self?.addAndPrintScore(tag: button.tag, score: 10)
+            }
+            let resetScore = UIAction(title: "점수 초기화", attributes: .destructive) { _ in
+                if let score = self?.countsOfEmojiTapped[button.tag] {
+                    self?.addAndPrintScore(
+                        tag: button.tag,
+                        score: score * -1
+                    )
+                }
+            }
+            
+            let menu = UIMenu(
+                title: "점수를 선택해주세요!",
+                image: nil,
+                identifier: nil,
+                options: .displayInline,
+                children: [plusOne, plusFive, plusTen, resetScore]
+            )
+            
+            button.menu = menu
+        }
     }
     
     @IBAction private func emojiButtonTapped(_ sender: UIButton) {
-        countsOfEmojiTapped[sender.tag] += 1
-        print(countsOfEmojiTapped[sender.tag])
+        addAndPrintScore(tag: sender.tag)
+    }
+    
+    private func addAndPrintScore(tag: Int, score: Int = 1) {
+        countsOfEmojiTapped[tag] += score
+        if let mood = Mood(rawValue: tag) {
+            print("\(mood.name): \(countsOfEmojiTapped[tag])점")
+        }
     }
 }
